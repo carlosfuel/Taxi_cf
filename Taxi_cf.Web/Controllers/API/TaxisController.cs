fd3d764ef7abe7f6e30320427f16716d85d72529
Carlos Alberto Fuel Tulcán <carlosfuel@gmail.com>
2020-02-29 12:56:20 -0500

    Proyecto Desarrollado hasta el video 13.  Tutorial 61 - Part 13 - API Controller 1. Febrero 29 de 2020.

---
+++ Taxi_cf.Web/Controllers/API/TaxisController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Taxi_cf.Web.Data;
+using Taxi_cf.Web.Data.Entities;
+
+namespace Taxi_cf.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaxisController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public TaxisController(DataContext context)
+        {
+            _context = context;
+        }
+        
+        // GET: api/Taxis/5
+        [HttpGet("{placa}")]
+        public async Task<IActionResult> GetTaxiEntity([FromRoute] string placa)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            TaxiEntity taxiEntity = 
+                await _context.Taxis.FirstOrDefaultAsync(t => t.Plaque ==  placa); // no funciona con valores enteros
+
+            if (taxiEntity == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(taxiEntity);
+        }
+    }
+}
