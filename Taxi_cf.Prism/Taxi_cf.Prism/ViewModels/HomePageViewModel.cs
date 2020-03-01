d0c1582aa972bb82bcfdc7f32c342c2ec0cd1cde
Carlos Alberto Fuel Tulcán <carlosfuel@gmail.com>
2020-03-01 07:40:25 -0500

    Proyecto Desarrollado hasta el Video 16. Tutorial 61 - Part 16 - Show map on App.

---
+++ Taxi_cf.Prism/Taxi_cf.Prism/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Prism.Navigation;
+namespace Taxi_cf.Prism.ViewModels
+{
+    public class HomePageViewModel : ViewModelBase
+    {
+        public HomePageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Taxi Qualifier";
+        }
+    }
+}
