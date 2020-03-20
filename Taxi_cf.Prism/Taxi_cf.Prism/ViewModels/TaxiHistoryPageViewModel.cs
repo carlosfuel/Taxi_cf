e35cfcbaa448daf5307c8518b63c641924348bb3
Carlos Alberto Fuel Tulcán <carlosfuel@gmail.com>
2020-03-20 05:20:46 -0500

    Desarrollo hasta el video 23, Tutorial 61 - Part 23 - Consume API 3. Marzo 20 de  2020.

--- Taxi_cf.Prism/Taxi_cf.Prism/ViewModels/TaxiHistoryPageViewModel.cs
+++ Taxi_cf.Prism/Taxi_cf.Prism/ViewModels/TaxiHistoryPageViewModel.cs
@@ -1,14 +1,68 @@
-﻿using Prism.Navigation;
+﻿using Prism.Commands;
+using Prism.Navigation;
+using System.Text.RegularExpressions;
+using Taxi_cf.Common.Models;
+using Taxi_cf.Common.Services;
 
 namespace Taxi_cf.Prism.ViewModels
 {
     public class TaxiHistoryPageViewModel : ViewModelBase
     {
+        private readonly IApiService _apiService;
+        private TaxiResponse _taxi;
+        private DelegateCommand _checkPlaqueCommand;
+
         public TaxiHistoryPageViewModel(
-            INavigationService navigationService)
-            : base(navigationService)
+            INavigationService navigationService,
+            IApiService apiService) : base(navigationService)
         {
-            Title = "See taxi history";
+            _apiService = apiService;
+            Title = "Taxi History";
+        }
+
+        public TaxiResponse Taxi
+        {
+            get => _taxi;
+            set => SetProperty(ref _taxi, value);
+        }
+
+        public string Plaque { get; set; }
+
+        public DelegateCommand CheckPlaqueCommand => _checkPlaqueCommand ?? (_checkPlaqueCommand = new DelegateCommand(CheckPlaqueAsync));
+
+        private async void CheckPlaqueAsync()
+        {
+            if (string.IsNullOrEmpty(Plaque))
+            {
+                await App.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a plaque.",
+                    "Accept");
+                return;
+            }
+
+            Regex regex = new Regex(@"^([A-Za-z]{3}\d{3})$");
+            if (!regex.IsMatch(Plaque))
+            {
+                await App.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "The plaque must start with three letters and end with three numbers.",
+                    "Accept");
+                return;
+            }
+
+            string url = App.Current.Resources["UrlAPI"].ToString();
+            Response response = await _apiService.GetTaxiAsync(Plaque, url, "api", "/Taxis");
+            if (!response.IsSuccess)
+            {
+                await App.Current.MainPage.DisplayAlert(
+                    "Error",
+                    response.Message,
+                    "Accept");
+                return;
+            }
+
+            Taxi = (TaxiResponse)response.Result;
         }
     }
 }
